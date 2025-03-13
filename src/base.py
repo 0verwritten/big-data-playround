087537fdf9a34032ff9d30a8ebdd6f1f43bad369
Vladyslav Moiseienko <42748460+0verwritten@users.noreply.github.com>
2025-03-13 18:32:00 +0200

    added typing

--- src/base.py
+++ src/base.py
@@ -2,16 +2,19 @@ from pyspark.sql import SparkSession
 import logging
 
 class BaseClass:
+    spark: SparkSession
+
     def __init__(self):
         logging.basicConfig(
             level=logging.INFO,
             format='%(asctime)s - %(levelname)s - %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S'
         )
-        self.spark = SparkSession.builder \
+        builder: SparkSession.Builder = SparkSession.builder
+        self.spark = builder \
             .appName("NYC Taxi Data Analysis") \
-        .config("spark.sql.shuffle.partitions", "8") \
-        .config("spark.executor.memory", "4g") \
+            .config("spark.sql.shuffle.partitions", "8") \
+            .config("spark.executor.memory", "4g") \
             .config("spark.sql.session.timeZone", "UTC") \
             .getOrCreate()
 
--- src/extraction/loaders.py
+++ src/extraction/loaders.py
@@ -1,16 +1,21 @@
 from ..base import BaseClass
 from ..transformation.cleaning import Cleaning
-class Loaders(BaseClass, Cleaning):
+from .schemas import Schemas
+
+
+class Loaders(BaseClass, Cleaning, Schemas):
     def load_trip_data(self):
         for i in range(1, 13):
             path = f"dataset/trip_data/trip_data_{i}.csv"
-            df = self.spark.read.csv(path, header=True, inferSchema=True)
-            
+            df = self.spark.read.csv(
+                path, header=True, schema=self.trip_data_schema, inferSchema=True
+            )
 
             output_path = f"dataset/trip_data_parquet"
             df.write.mode("append").parquet(output_path)
 
-        df = self.spark.read.parquet(output_path)
-        self.clean_trip_data()
-        
+        df = self.spark.read.parquet(output_path).schema(self.trip_data_schema)
+        cleaned_df = self.clean_trip_data(df)
 
+        cleaned_output_path = f"dataset/trip_data_cleaned_parquet"
+        cleaned_df.write.mode("overwrite").parquet(cleaned_output_path)
