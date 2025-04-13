887c60442b1a65d6904eaaefc87862677916d053
Khrystyna Levonova <113831974+klevonova@users.noreply.github.com>
2025-04-13 22:53:43 +0300

    improved cleaning

--- src/transformation/cleaning.py
+++ src/transformation/cleaning.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, year, month, dayofmonth, hour, dayofweek, when
 from ..base import BaseClass
 
 class Cleaning(BaseClass):
@@ -7,23 +7,36 @@ class Cleaning(BaseClass):
         self.logger.info("Очищення даних поїздок...")
 
         cleaned_df = trip_df.filter(
-            (col(" pickup_datetime").isNotNull())
+            (col("medallion").isNotNull()) &
+            (col("hack_license").isNotNull()) &
+            (col("vendor_id").isNotNull()) &
+            (col("pickup_datetime").isNotNull())
         )
 
         cleaned_df = cleaned_df.filter(
-            (col(" passenger_count") >= 1)
+            (col("passenger_count") >= 1) &
+            (col("passenger_count") <= 9)
         )
 
         cleaned_df = cleaned_df.filter(
-            (col(" trip_distance") > 0)
+            (col("trip_distance") > 0) &
+            (col("trip_distance") < 100) # roughly
         )
 
         cleaned_df = cleaned_df.filter(
-            (col(" pickup_longitude") != 0) &
-            (col(" pickup_latitude") != 0) &
-            (col(" dropoff_longitude") != 0) &
-            (col(" dropoff_latitude") != 0)
+            (col("pickup_longitude") != 0) &
+            (col("pickup_latitude") != 0) &
+            (col("dropoff_longitude") != 0) &
+            (col("dropoff_latitude") != 0)
         )
+
+        cleaned_df = cleaned_df.filter((col("trip_time_in_secs") > 0) & (col('trip_distance') > 0))
+
+        cleaned_df = cleaned_df.withColumn("pickup_year", year(col("pickup_datetime")))
+        cleaned_df = cleaned_df.withColumn("pickup_month", month(col("pickup_datetime")))
+        cleaned_df = cleaned_df.withColumn("pickup_day", dayofmonth(col("pickup_datetime")))
+        cleaned_df = cleaned_df.withColumn("pickup_hour", hour(col("pickup_datetime")))
+        cleaned_df = cleaned_df.withColumn("pickup_dayofweek", dayofweek(col("pickup_datetime")))
 
         original_count = trip_df.count()
         cleaned_count = cleaned_df.count()
@@ -37,12 +50,25 @@ class Cleaning(BaseClass):
     def clean_fare_data(self, fare_df: DataFrame) -> DataFrame:
         self.logger.info("Очищення даних оплати...")
 
-        cleaned_df = cleaned_df.filter(
+        cleaned_df = fare_df.filter(
+            (col("medallion").isNotNull()) &
+            (col("hack_license").isNotNull()) &
+            (col("vendor_id").isNotNull()) &
+            (col("pickup_datetime").isNotNull())
+        )
+
+        cleaned_df = fare_df.filter(
             (col("fare_amount") >= 0)
         )
 
         cleaned_df = cleaned_df.filter(
             (col("total_amount") >= 0)
+        )
+
+        cleaned_df = cleaned_df.withColumn(
+            "payment_type",
+            when(col("payment_type").isNull(), "UNKNOWN")
+            .otherwise(col("payment_type"))
         )
 
         original_count = fare_df.count()
